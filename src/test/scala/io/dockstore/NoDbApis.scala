57ec2195cda5c789b47d3f40dbd5a710f67302d5
Charles Overbeck <coverbec@ucsc.edu>
2019-02-14 15:24:29 -0800

    Add description to HTML and super-fast tests
    * Add a description to the generated HTML report
    * Create a very simple test that calls APIs whose implementation
    doesn't require any database access; use it to determine how
    many requests can simultaneously be handled if you remove
    database access from the equation.

---
+++ src/test/scala/io/dockstore/NoDbApis.scala
@@ -0,0 +1,15 @@
+package io.dockstore
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+object NoDbApis {
+
+  val simple = exec(
+    Requests.Ga4gh2.getMetadata
+      .check(status is 200)
+  ).exec(
+    Requests.MetaData.getSourceControlList
+      .check(status is 200)
+  )
+}
